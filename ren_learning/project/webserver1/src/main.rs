0fd74de3d2542c83d5d51aca10e7d3e7f105a7a0
Vibhatha Lakmal Abeykoon <vibhatha@gmail.com>
2026-05-04 21:34:41 +0530

    Add functional version of multi-threaded worker model

--- ren_learning/project/webserver1/src/main.rs
+++ ren_learning/project/webserver1/src/main.rs
@@ -12,36 +12,8 @@ enum ServerStatus {
     Error(String),
 }
 
-pub struct ThreadPool {
-    threads: Vec<thread::JoinHandle<()>>,
-}
+use webserver1::ThreadPool;
 
-impl ThreadPool {
-    /// Create a new ThreadPool.
-    ///
-    /// The size is the number of threads in the pool.
-    ///
-    /// # Panics
-    ///
-    /// The `new` function will panic if the size is zero.
-    pub fn new(size: usize) -> ThreadPool {
-        assert!(size > 0);
-
-        let mut threads = Vec::with_capacity(size);
-        for _ in 0..size {
-            // create some threads and store them in a vector
-        }
-
-        ThreadPool { threads }
-    }
-
-    pub fn execute<F>(&self, f: F) 
-    where
-        F: FnOnce() + Send + 'static,
-    {
-
-    }
-}
 
 fn start_server(host: &String, port: &String) -> ServerStatus {
     // Good if we can validate the host and port
